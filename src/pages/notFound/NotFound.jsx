1d4c0b3297095335d8a4a27ccc485d986537e3c3
Kevin <pintokevin2002@hotmail.com>
2025-01-24 18:55:42 +0100

    Adding a 404 page

---
+++ src/pages/notFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="not-found-container">
+            <h1>
+                404 - Not Found
+            </h1>
+            <p>
+                The page you are looking for might have been removed or is temporarily unavailable.
+            </p>
+        </div>
+    );
+};
+
+export default NotFound;
