4650fb76493d423b26863bf3ae85f925d4e62b8a
Kevin <pintokevin2002@hotmail.com>
2025-01-24 16:20:59 +0100

    simplified the carousel

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,26 +7,24 @@ const Hero = ({movies}) => {
         <div className='movie-carousel-container'>
             <Carousel>
                 {
-                    movies?.map((movie) => {
-                        return (
-                            <Paper key={movie.imdbId}>
-                                <div className='movie-card-container'>
-                                    <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
-                                        <div className="movie-detail">
-                                            <div className="movie-poster">
-                                                <img src={movie.poster} loading="lazy" alt="" />
-                                            </div>
-                                            <div className="movie-title">
-                                                <h4>
-                                                    {movie.title}
-                                                </h4>
-                                            </div>
+                    movies?.map((movie) => (
+                        <Paper key={movie.imdbId}>
+                            <div className='movie-card-container'>
+                                <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                    <div className="movie-detail">
+                                        <div className="movie-poster">
+                                            <img src={movie.poster} loading="lazy" alt="" />
+                                        </div>
+                                        <div className="movie-title">
+                                            <h4>
+                                                {movie.title}
+                                            </h4>
                                         </div>
                                     </div>
                                 </div>
-                            </Paper>
-                        )
-                    })
+                            </div>
+                        </Paper>
+                    ))
                 }
             </Carousel>
         </div>
