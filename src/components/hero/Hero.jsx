315b3206d782da8bdbe2e6edd484c2baf5c2ff3b
Kevin <pintokevin2002@hotmail.com>
2025-01-24 16:41:40 +0100

    Updating the css var name

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = ({movies}) => {
                     movies?.map((movie) => (
                         <Paper key={movie.imdbId}>
                             <div className='movie-card-container'>
-                                <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className="movie-card" style={{"--_img": `url(${movie.backdrops[0]})`}}>
                                     <div className="movie-detail">
                                         <div className="movie-poster">
                                             <img src={movie.poster} loading="lazy" alt="" />
