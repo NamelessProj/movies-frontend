2542dc82a27834e7b81509df2811e39d6dc6f0c2
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2025-01-24 15:57:46 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import api from './api/axiosConfig.js';
+import {useEffect, useState} from "react";
+import Layout from "./components/Layout.jsx";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+
+function App() {
+
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = async () => {
+        try{
+            const response = await api.get("/movies");
+            setMovies(response.data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+  return (
+    <div className="App">
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home movies={movies} />} />
+            </Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "../../components/hero/Hero";
+
+
+const Home = ({movies}) => {
+    return (
+        <>
+            <Hero movies={movies} />
+        </>
+    );
+};
+
+export default Home;
