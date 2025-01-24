01fa1e08f8b2baea8c3778017010ae7e37403f7b
Kevin <pintokevin2002@hotmail.com>
2025-01-24 17:35:19 +0100

    Adding a link to the trailer page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-material-ui-carousel';
 import {Paper} from "@mui/material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
 
 const Hero = ({movies}) => {
     return (
@@ -23,9 +24,11 @@ const Hero = ({movies}) => {
                                             </h4>
                                         </div>
                                         <div className="movie-buttons-container">
-                                            <div className="play-button-icon-container">
-                                                <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay} />
-                                            </div>
+                                            <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                <div className="play-button-icon-container">
+                                                    <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay}/>
+                                                </div>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
